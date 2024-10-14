b2ea965782555f7e9fc40091f41898d374f1b5fb
Samuel do nascimento ramos <hmsrp.ramos7@gmail.com>
2024-10-14 17:07:12 -0300

    Add files via upload

---
+++ script8.js
@@ -0,0 +1,4 @@
+let fahrenheit = parseInt(prompt("digite o valor"))
+let celsius = parseInt(prompt("digite o valor"))
+let celsius_result = (fahrenheit - 32) * 5 / 9
+alert ("a temperatura em celsius é:" + celsius_result)
